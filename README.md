bc705266dd557f702b36a385f7d79dc0d952637b
Enwemasor Barnabas <qhodeweb@gmail.com>
2021-03-02 11:58:26 +0100

    initial update

---
+++ README.md
@@ -0,0 +1,16 @@
+Get perfect shadow for you divs
+
+#install
+
+npm i shadowwizzard -save
+or
+yarn add shadowwizzard
+
+```
+import {shadowwizard} from 'shadowwizard'
+
+shadowwizard({
+  shadow_type = "soft",  // 'soft'|'hard'|null
+  padding: true
+})
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+function shadowwizard(options) {
+  let images = document.querySelectorAll(".shadowwizard");
+  if (options.shadow_type === "hard") {
+    options.shadow_type = "0px";
+  } else {
+    options.shadow_type = "15px";
+  }
+  images.forEach((image) => {
+    image.getElementsByClassName.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+    if (options.style.padding) {
+      image.style.padding = "1rem";
+    }
+  });
+}
+
+module.exports.shadowwizard = shadowwizard;
